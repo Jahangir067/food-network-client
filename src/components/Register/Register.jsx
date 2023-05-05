bbb4025a06729585283e0c8d0cb05ac28a00d014
Jahangir067 <dm.jahangir06@gmail.com>
2023-05-05 19:10:58 +0600

    register update

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../Provider/AuthProvider';
+// Register section
 
 const Register = () => {
     const [error, setError] = useState('')
